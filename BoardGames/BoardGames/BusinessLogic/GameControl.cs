692c08515b49f6c4e266aa449e6a953f99b5fb7f
Simon Andersen <10195871@ucn.dk>
2023-09-10 00:11:24 +0200

    Forbedringer vha. try catch

--- BoardGames/BoardGames/BusinessLogic/GameControl.cs
+++ BoardGames/BoardGames/BusinessLogic/GameControl.cs
@@ -22,52 +22,151 @@ namespace BoardGames.BusinessLogic {
             return foundGame;
         }
 
-        public MergedGame GetGameByNoOfPlayers(int no) {
-            MergedGame? foundMergedGame = new MergedGame();
-            List<Game>? foundGames = null;
-            List<GameVersion>? foundGameVersion = null;
-            List<GameCharacter>? foundGameCharacter = null;
-            Game gameToReturn;
-            GameVersion versionToReturn;
-            List<GameCharacter>? charactersToReturn = new List<GameCharacter>();
-            int randomGameIndex;
-            int randomVersionIndex;
-            int gameIdInt;
-            int versionIdInt;
+    public MergedGame GetGameByNoOfPlayers(int no) {
+        List<Game>? foundGames = null;
+        List<GameVersion>? foundVersions = null;
+        List<GameCharacter>? foundCharacters = null;
 
-            if (no > 0) {
-                foundGames = _gameAccess.GetGameByNoOfPlayers(no);  //Finder hele listen af games
-                    randomGameIndex = new Random().Next(foundGames.Count);  //Tager et random index på listen
-                if (randomGameIndex > 0) {
-                    gameToReturn = foundGames.ElementAt(randomGameIndex);  //Finder game på det index
-                    gameIdInt = gameToReturn.GameId;  //Assigner det fundne game.GameId til gameIdInt
+        Game gameToMerge = null;
+        GameVersion versionToMerge = null;
+        List<GameCharacter> charactersToMerge = new List<GameCharacter>();
 
-                    foundGameVersion = _versionAccess.GetVersionByGameId(gameIdInt);  //Finder hele listen af versioner med specifikt GameId
-                    randomVersionIndex = new Random().Next(foundGameVersion.Count);  //Tager et random index på listen
-                    if (randomVersionIndex > 0) {
-                        versionToReturn = foundGameVersion.ElementAt(randomVersionIndex);  //Finder version på det index
-                        versionIdInt = versionToReturn.VersionId;  //Assigner det fundne Version.VersionId til versionIdInt
+        GameCharacter tempCharacter = null;
+        MergedGame foundMergedGame = new MergedGame();
+        int randomIndex = -1;
 
-                        foundGameCharacter = _characterAccess.GetCharacterByVersionId(versionIdInt);  //Finder hele listen af characters med specifikt versionId
-                        if (foundGameCharacter.Count != 0) {
-                            int i = 1;
-                            while (i <= no) {
-                                int randomCharacterIndex = new Random().Next(foundGameCharacter.Count); //Finder random index på listen
-                                GameCharacter characterAtIndex = foundGameCharacter.ElementAt(randomCharacterIndex);  //Finder character på index
-                                if (characterAtIndex != null && !charactersToReturn.Contains(characterAtIndex)) { //Går videre hvis den findne character ikke er null og ikke allerede er på listen
-                                    charactersToReturn.Add(characterAtIndex); //Tilføjer den fundne character til charactersToReturn
-                                    i++;
-                                }
-                            }
-                        }
-                        foundMergedGame.Game = gameToReturn;
-                        foundMergedGame.GameVersion = versionToReturn;
-                        foundMergedGame.GameCharacter = charactersToReturn;
+        try {
+            foundGames = _gameAccess.GetGameByNoOfPlayers(no);
+            randomIndex = new Random().Next(foundGames.Count);
+            gameToMerge = foundGames.ElementAt(randomIndex);
+
+            randomIndex = -1;
+
+            foundVersions = _versionAccess.GetVersionByGameId(gameToMerge.GameId);
+            randomIndex = new Random().Next(foundVersions.Count);
+            versionToMerge = foundVersions.ElementAt(randomIndex);
+
+            randomIndex = -1;
+
+            foundCharacters = _characterAccess.GetCharacterByVersionId(versionToMerge.VersionId);
+            int i = 1;
+            while (i <= no) {
+                randomIndex = new Random().Next(foundCharacters.Count);
+                if (randomIndex >= 0) {
+                    tempCharacter = foundCharacters.ElementAt(randomIndex);
+                    if (tempCharacter != null && !charactersToMerge.Contains(tempCharacter)) {
+                        charactersToMerge.Add(tempCharacter);
+                        randomIndex = -1;
+                        i++;
                     }
                 }
             }
-            return foundMergedGame;
+            foundMergedGame.Game = gameToMerge;
+            foundMergedGame.GameVersion = versionToMerge;
+            foundMergedGame.GameCharacter = charactersToMerge;
+        } catch {
+            Console.WriteLine("No games found");
         }
+        return foundMergedGame;
+    }
+
+        //public MergedGame GetGameByNoOfPlayers(int no) {
+        //    List<Game>? foundGames = null;              //
+        //    List<GameVersion>? foundVersions = null;    // LIST AF GAMES, VERSIONER OG KARAKTERER
+        //    List<GameCharacter>? foundCharacters = null;//
+
+        //    int randomIndex = -1;
+
+        //    Game gameToMerge = null;                                            // FUNDET GAME SOM SKAL MERGES TIL MergedGame
+        //    GameVersion versionToMerge = null;                                  // FUNDET VERSION SOM SKAL MERGES TIL MergedGame
+        //    List<GameCharacter> charactersToMerge = new List<GameCharacter>();  // FUNDET KARAKTERER SOM SKAL MERGES TIL MergedGame
+
+        //    GameCharacter tempCharacter = null;             // TEMP KARAKTER FUNDET TILFÆLDIGT
+
+        //    MergedGame foundMergedGame = new MergedGame();
+
+        //    if (no > 0) {
+        //        foundGames = _gameAccess.GetGameByNoOfPlayers(no);  //
+        //        randomIndex = new Random().Next(foundGames.Count);  // DETTE STYKKE FINDER
+        //        if (randomIndex >= 0) {                              // ET TILFÆLDIGT SPIL
+        //            gameToMerge = foundGames.ElementAt(randomIndex);//
+
+        //            randomIndex = -1;
+
+        //            foundVersions = _versionAccess.GetVersionByGameId(gameToMerge.GameId);  //
+        //            randomIndex = new Random().Next(foundVersions.Count);                   // DETTE STYKKE FINDER
+        //            if (randomIndex >= 0) {                                                  // EN TILFÆLDIG VERSION
+        //                versionToMerge = foundVersions.ElementAt(randomIndex);              //
+
+        //                randomIndex = -1;
+
+        //                foundCharacters = _characterAccess.GetCharacterByVersionId(versionToMerge.VersionId);
+        //                if (foundCharacters.Count > 0) {
+        //                    int i = 1;
+        //                    while (i <= no) {
+        //                        randomIndex = new Random().Next(foundCharacters.Count);
+        //                        if (randomIndex >= 0) {
+        //                            tempCharacter = foundCharacters.ElementAt(randomIndex);
+        //                            if (tempCharacter != null && !charactersToMerge.Contains(tempCharacter)) {
+        //                                charactersToMerge.Add(tempCharacter);
+        //                                randomIndex = -1;
+        //                                i++;
+        //                            }
+        //                        }
+        //                    }
+        //                    foundMergedGame.Game = gameToMerge;
+        //                    foundMergedGame.GameVersion = versionToMerge;
+        //                    foundMergedGame.GameCharacter = charactersToMerge;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return foundMergedGame;
+        //}
+
+        //public MergedGame GetGameByNoOfPlayers(int no) {
+        //    MergedGame? foundMergedGame = new MergedGame();
+        //    List<Game>? foundGames = null;
+        //    List<GameVersion>? foundGameVersion = null;
+        //    List<GameCharacter>? foundGameCharacter = null;
+        //    Game gameToReturn;
+        //    GameVersion versionToReturn;
+        //    List<GameCharacter>? charactersToReturn = new List<GameCharacter>();
+        //    int randomGameIndex;
+        //    int randomVersionIndex;
+        //    int gameIdInt;
+        //    int versionIdInt;
+
+        //    if (no > 0) {
+        //        foundGames = _gameAccess.GetGameByNoOfPlayers(no);
+        //            randomGameIndex = new Random().Next(foundGames.Count);
+        //            gameToReturn = foundGames.ElementAt(randomGameIndex);
+        //            gameIdInt = gameToReturn.GameId;
+
+        //            foundGameVersion = _versionAccess.GetVersionByGameId(gameIdInt);
+        //            randomVersionIndex = new Random().Next(foundGameVersion.Count);
+
+        //            versionToReturn = foundGameVersion.ElementAt(randomVersionIndex);
+        //            versionIdInt = versionToReturn.VersionId;
+
+        //            foundGameCharacter = _characterAccess.GetCharacterByVersionId(versionIdInt);
+        //            if (foundGameCharacter.Count != 0) {
+        //                int i = 1;
+        //                while (i <= no) {
+        //                    int randomCharacterIndex = new Random().Next(foundGameCharacter.Count);
+        //                    GameCharacter characterAtIndex = foundGameCharacter.ElementAt(randomCharacterIndex);
+        //                    if (characterAtIndex != null && !charactersToReturn.Contains(characterAtIndex)) {
+        //                        charactersToReturn.Add(characterAtIndex);
+        //                        i++;
+        //                    }
+        //                }
+        //            }
+        //            foundMergedGame.Game = gameToReturn;
+        //            foundMergedGame.GameVersion = versionToReturn;
+        //            foundMergedGame.GameCharacter = charactersToReturn;
+        //    }
+        //    return foundMergedGame;
+        //}
 
         public List<Game> GetAll() {
             return _gameAccess.GetAll();
